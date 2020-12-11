f61489e03604a8ea2a4b0c547d7562782b14f743
Maximilian Muecke <muecke.maximilian@gmail.com>
2020-12-11 22:47:25 +0100

    generalized ddm_sim

--- R/ddm_sim.R
+++ R/ddm_sim.R
@@ -1,32 +1,43 @@
-ddm_sim_norm <- function(price, dividend, g_mu, g_sigma, n_sim = 1000) {
-  # monte carlo sim for ddm model with normal distribution of growth rate
-  g <- rnorm(n_sim, mean = g_mu, sd = g_sigma)
-  share_values <- numeric(length(n_sim))
-  for (i in 1:n_sim) {
-    g_t <- sample(g, size = 1, replace = TRUE)
-    value_per_share <- price / (dividend - g_t)
-    share_values[i] <- value_per_share
+#' Monte Carlo Simulation for the Discount Dividend model
+#'
+#' @param price 
+#' @param dividend 
+#' @param g_mu 
+#' @param g_sigma 
+#' @param n_sim 
+#' @param distribution 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ddm_sim <- function(price, dividend, g_mu = NULL, g_sigma = NULL, g_min = NULL,
+                    g_max = NULL, n_sim = 1000,
+                    dist = c("normal", "triangle", "uniform")) {
+  distribution <- match.arg(distribution)
+  if (distribution == "normal") {
+    if (!is.null(g_mu) && !is.null(g_sigma)) {
+      g <- rnorm(n_sim, mean = g_mu, sd = g_sigma)
+    } else {
+      stop("Mean and/or standard deviation is missing for normal distribution.")
+    }
+  } else if (distribution == "triangle") {
+    if (!is.null(g_min) && !is.null(g_max)) {
+      g <- triangle::rtriangle(n_sim, a = g_min, b = g_max)
+    } else {
+      stop("Minimum and/or maximium is missing for triangle distribution.")
+    }
+  } else if (distribution == "uniform") {
+    if (!is.null(g_min) && !is.null(a)) {
+      g <- runif(n_sim, min = g_min, max = g_max)
+    } else {
+      stop("Minimum and/or maximium is missing for uniform distribution.")
+    }
+  } else {
+    stop("Selected a distribution that is not available.")
   }
-  share_values
-}
-
-ddm_sim_tri <- function(price, dividend, g_min, g_max, n_sim = 1000) {
-  # monte carlo sim for ddm model with triangular distribution of growth rate
-  g <- triangle::rtriangle(n_sim, a = g_min, b = g_max)
   share_values <- numeric(length(n_sim))
-  for (i in 1:n_sim) {
-    g_t <- sample(g, size = 1, replace = TRUE)
-    value_per_share <- price / (dividend - g_t)
-    share_values[i] <- value_per_share
-  }
-  share_values
-}
-
-ddm_sim_unif <- function(price, dividend, g_min, g_max, n_sim = 1000) {
-  # monte carlo sim for ddm model with uniform distribution of growth rate
-  g <- runif(n_sim, min = g_min, max = g_max)
-  share_values <- numeric(length(n_sim))
-  for (i in 1:n_sim) {
+  for (i in seq_len(n_sim)) {
     g_t <- sample(g, size = 1, replace = TRUE)
     value_per_share <- price / (dividend - g_t)
     share_values[i] <- value_per_share
