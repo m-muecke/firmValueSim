30bfdb96f2e191fafe56ea50cf1c87d3be45ceea
Maximilian Muecke <muecke.maximilian@gmail.com>
2020-12-12 15:55:50 +0100

    added documentation for dividend discount model

--- R/ddm_sim.R
+++ R/ddm_sim.R
@@ -1,19 +1,22 @@
-#' Monte Carlo Simulation for the Discount Dividend model
+#' Monte Carlo Simulation for the dividend discount model (Gordon Growth model)
 #'
-#' @param price 
-#' @param dividend 
-#' @param g_mu 
-#' @param g_sigma 
-#' @param n_sim 
-#' @param distribution 
+#' @param price The stock price of the company.
+#' @param dividend The distributed dividend for the stock.
+#' @param g_mu The mean of the normal distribution.
+#' @param g_sigma The standard deviation of the normal distribution.
+#' @param n_sim The number of simulations.
+#' @param distribution The type of distribution.
+#' @param g_min Lower limit of the distribution.
+#' @param g_max Upper limit of the distribution.
 #'
-#' @return
+#' @return A numeric vector with the current simulated stock prices.
 #' @export
 #'
 #' @examples
+#' dividends <- ddm_sim(25.0, 1.5, g_mu = 0.03, g_sigma = 0.01)
 ddm_sim <- function(price, dividend, g_mu = NULL, g_sigma = NULL, g_min = NULL,
                     g_max = NULL, n_sim = 1000,
-                    dist = c("normal", "triangle", "uniform")) {
+                    distribution = c("normal", "triangle", "uniform")) {
   distribution <- match.arg(distribution)
   if (distribution == "normal") {
     if (!is.null(g_mu) && !is.null(g_sigma)) {
@@ -36,7 +39,7 @@ ddm_sim <- function(price, dividend, g_mu = NULL, g_sigma = NULL, g_min = NULL,
   } else {
     stop("Selected a distribution that is not available.")
   }
-  share_values <- numeric(length(n_sim))
+  share_values <- numeric(n_sim)
   for (i in seq_len(n_sim)) {
     g_t <- sample(g, size = 1, replace = TRUE)
     value_per_share <- price / (dividend - g_t)
