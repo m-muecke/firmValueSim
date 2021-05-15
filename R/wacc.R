1e99747a582dca14caad7986b0d49bae447b3254
Maximilian Muecke <muecke.maximilian@gmail.com>
2021-05-15 16:32:14 +0200

    fixed pipeline and added loading func for fundamental growth rate in EPS data

--- R/wacc.R
+++ R/wacc.R
@@ -1,6 +1,6 @@
 #' Load cost of capital data
 #'
-#' `load_wacc_data` loads Aswath Damodaran's latest cost of capital by industry
+#' `load_wacc` loads Aswath Damodaran's latest cost of capital by industry
 #' data.
 #'
 #' @param region 
@@ -9,8 +9,8 @@
 #' @export
 #'
 #' @examples
-load_wacc_data <- function(region = c("US", "Europe", "Japan", "China", "India",
-                                      "Global")) {
+load_wacc <- function(region = c("US", "Europe", "Japan", "China", "India",
+                                 "Emerging Markets", "Global")) {
   region <- match.arg(region)
   url_list <- list(
     US = "http://www.stern.nyu.edu/~adamodar/pc/datasets/wacc.xls",
@@ -18,6 +18,7 @@ load_wacc_data <- function(region = c("US", "Europe", "Japan", "China", "India",
     Japan = "http://www.stern.nyu.edu/~adamodar/pc/datasets/waccJapan.xls",
     China = "http://www.stern.nyu.edu/~adamodar/pc/datasets/waccChina.xls",
     India = "http://www.stern.nyu.edu/~adamodar/pc/datasets/waccIndia.xls",
+    `Emerging Markets` = "http://www.stern.nyu.edu/~adamodar/pc/datasets/waccemerg.xls",
     Global = "http://www.stern.nyu.edu/~adamodar/pc/datasets/waccGlobal.xls"
   )
   temp <- tempfile()
@@ -30,7 +31,7 @@ load_wacc_data <- function(region = c("US", "Europe", "Japan", "China", "India",
 
 #' Load total beta data
 #'
-#' `load_beta_data` loads Aswath Damodaran's latest total beta by industry
+#' `load_beta` loads Aswath Damodaran's latest total beta by industry
 #' data.
 #' 
 #' @param region 
@@ -39,8 +40,8 @@ load_wacc_data <- function(region = c("US", "Europe", "Japan", "China", "India",
 #' @export
 #'
 #' @examples
-load_beta_data <- function(region = c("US", "Europe", "Japan", "China", "India",
-                                      "Global")) {
+load_beta <- function(region = c("US", "Europe", "Japan", "China", "India",
+                                 "Emerging Markets", "Global")) {
   region <- match.arg(region)
   url_list <- list(
     US = "http://www.stern.nyu.edu/~adamodar/pc/datasets/totalbeta.xls",
@@ -48,12 +49,42 @@ load_beta_data <- function(region = c("US", "Europe", "Japan", "China", "India",
     Japan = "http://www.stern.nyu.edu/~adamodar/pc/datasets/totalbetaJapan.xls",
     China = "http://www.stern.nyu.edu/~adamodar/pc/datasets/totalbetaChina.xls",
     India = "http://www.stern.nyu.edu/~adamodar/pc/datasets/totalbetaIndia.xls",
+    `Emerging Markets` = "http://www.stern.nyu.edu/~adamodar/pc/datasets/totalbetaemerg.xls",
     Global = "http://www.stern.nyu.edu/~adamodar/pc/datasets/totalbetaGlobal.xls"
   )
   temp <- tempfile()
   download.file(url_list[[region]], temp, mode = "wb", quiet = TRUE)
   df <- readxl::read_excel(
     temp, range = "A8:G104"
+  )
+  df
+}
+
+#' Load fundamental growth rate in EPS data
+#' 
+#' `load_eps_growth_rate` loads Aswath Damodaran's latest fundamental growth
+#' rate in EPS by industry data.
+#'
+#' @param region 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_eps_growth_rate <- function(region = c("US", "Europe", "Japan",
+                                            "Emerging Markets", "Global")) {
+  region <- match.arg(region)
+  url_list <- list(
+    US = "http://www.stern.nyu.edu/~adamodar/pc/datasets/fundgr.xls",
+    Europe = "http://www.stern.nyu.edu/~adamodar/pc/datasets/fundgrEurope.xls",
+    Japan = "http://www.stern.nyu.edu/~adamodar/pc/datasets/fundgrJapan.xls",
+    `Emerging Markets` = "http://www.stern.nyu.edu/~adamodar/pc/datasets/fundgremerg.xls",
+    Global = "http://www.stern.nyu.edu/~adamodar/pc/datasets/fundgrGlobal.xls"
+  )
+  temp <- tempfile()
+  download.file(url_list[[region]], temp, mode = "wb", quiet = TRUE)
+  df <- readxl::read_excel(
+    temp, range = "A8:E104"
   )
   df
 }
